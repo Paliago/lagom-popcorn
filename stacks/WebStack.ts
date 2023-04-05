ef98c3ff7d47bed5d3ad0d537726ae44541dd49c
Alvin Johansson <alvin.johansson@elva-group.com>
2023-04-05 14:34:15 +0200

    chore: minor updates

--- stacks/WebStack.ts
+++ stacks/WebStack.ts
@@ -1,6 +1,7 @@
 import { AstroSite, Config, StackContext } from "sst/constructs";
 
 export default function WebStack({ stack }: StackContext) {
+  const AUTH_SECRET = new Config.Secret(stack, "AUTH_SECRET");
   const DISCORD_CLIENT_ID = new Config.Secret(stack, "DISCORD_CLIENT_ID");
   const DISCORD_CLIENT_SECRET = new Config.Secret(
     stack,
@@ -10,7 +11,7 @@ export default function WebStack({ stack }: StackContext) {
   // Create the Astro site
   const site = new AstroSite(stack, "Site", {
     path: "lagom-web/",
-    bind: [DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET],
+    bind: [AUTH_SECRET, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET],
   });
 
   // Add the site's URL to stack output
