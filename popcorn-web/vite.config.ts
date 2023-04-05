67a78fab099df86b303fb10b9ca6147a11ed62b1
Alvin Johansson <alvin.johansson@elva-group.com>
2023-04-05 09:17:25 +0200

    chore: the last solid changes

--- popcorn-web/vite.config.ts
+++ popcorn-web/vite.config.ts
@@ -3,5 +3,5 @@ import aws from "solid-start-sst";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ ssr: true, adapter: aws({ edge: false }) })],
+  plugins: [solid({ adapter: aws() })],
 });
